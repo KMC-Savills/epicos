33fe6910e56736a9775f02a8d5feaafe7b9ce629
Jezreel Pilapil <jezreel.pilapil@kmcmaggroup.com>
2019-11-20 16:10:09 +0800

    Created Stored Procedure, BookManager, BookRepository for Book

---
+++ Managers/BookManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EpicOS.Helpers;
+using EpicOS.Models.Entities;
+using EpicOS.Repository;
+namespace EpicOS.Managers
+{
+    public class BookManager : BaseManager
+    {
+        private BookRepository book;
+
+        public BookManager()
+        {
+            this.book = new BookRepository();
+        }
+        public List<Book> GetAll()
+        {
+            List<Book> books = cacheNinja.cache["usp_Book_GetAll"] as List<Book>;
+            if (books == null)
+            {
+                books = book.GetAll();
+                cacheNinja.cache.Set("User_GetAll", books, cacheNinja.cacheExpiry);
+            }
+            return books;
+        }
+        public Book GetByID(int id)
+        {
+            return GetAll().FirstOrDefault(e => e.ID.Equals(id));
+        }
+
+        public Result Insert(User parameter)
+        {
+            return book.Insert(parameter);
+        }
+
+        public Result Update(User parameter)
+        {
+            return book.Update(parameter);
+        }
+    }
+}
