7adcd366d8f8a7b7a67a2d6cbfc0993526c54ea9
Jorel Jaictin <jorel.jaictin@kmcmaggroup.com>
2019-11-22 17:49:42 +0800

    Commit Office Controller

--- Controllers/OfficeController.cs
+++ Controllers/OfficeController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using EpicOS.Models.Entities;
 using EpicOS.Managers;
 using Microsoft.AspNetCore.Mvc;
+using EpicOS.Repository;
+using EpicOS.Helpers;
 
 namespace EpicOS.Controllers
 {
@@ -12,11 +14,80 @@ namespace EpicOS.Controllers
     {
         public IActionResult Index()
         {
-            List<Office> viewModel = new List<Office>();
             OfficeManager manager = new OfficeManager();
-            viewModel = manager.OfficeGetAll();
+            return View(manager.OfficeGetAll());
+        }
 
-            return View(viewModel);
+        public IActionResult OfficeList()
+        {
+            OfficeManager manager = new OfficeManager();
+            return View(manager.OfficeGetAll());
+        }
+
+        public ActionResult Details(Office office)
+        {
+            /*OfficeRepository officeRepo = new OfficeRepository();
+            Office office = officeRepo.OfficeGetByID(id);
+
+            OfficeManager manager = new OfficeManager();
+
+            manager.OfficeUpdate(office);
+
+            return View(office);*/
+            OfficeManager manager = new OfficeManager();
+
+            manager.OfficeGetByID(office.ID);
+
+            return View(office);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOffice(Office office)
+        {
+            OfficeManager manager = new OfficeManager();
+            manager.OfficeInsert(office);
+
+            CacheNinja cache = new CacheNinja();
+            cache.ClearCache("Office_GetAll");
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            OfficeRepository officeRepo = new OfficeRepository();
+            Office office = officeRepo.OfficeGetByID(id);
+
+            OfficeManager manager = new OfficeManager();
+            manager.OfficeUpdate(office);
+            return View(office);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Office office)
+        {
+            OfficeManager manager = new OfficeManager();
+            manager.OfficeUpdate(office);
+
+            CacheNinja cache = new CacheNinja();
+            cache.ClearCache("Office_GetAll");
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            OfficeManager manager = new OfficeManager();
+            manager.Delete(Id);
+            return RedirectToAction("Index");
         }
     }
 }
